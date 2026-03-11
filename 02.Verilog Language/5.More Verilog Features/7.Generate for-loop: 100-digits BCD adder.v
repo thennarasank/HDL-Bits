f7e4011c6f21ae992a13f272a70cfba037790c0c
Thennarasan K <thennarasan.asus@gmail.com>
2026-03-11 19:35:41 +0530

    Update 7.Generate for-loop: 100-digits BCD adder.v

--- 02.Verilog Language/5.More Verilog Features/7.Generate for-loop: 100-digits BCD adder.v
+++ 02.Verilog Language/5.More Verilog Features/7.Generate for-loop: 100-digits BCD adder.v
@@ -5,7 +5,7 @@ module top_module(
     output [399:0] sum );
     wire [100:0] carry;
     assign carry[0] = cin;
-    genvar i;
+    genvar i; 
     generate
         for (i = 0; i < 100; i = i + 1) begin : bcd_chain
             bcd_fadd u_bcd (.a(a[i*4 +: 4]),.b(b[i*4 +: 4]),.cin(carry[i]),.cout(carry[i+1]),.sum(sum[i*4 +: 4]));
